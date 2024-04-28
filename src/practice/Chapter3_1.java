3367483c08ecc9611ea8f3aa16609fd2f71c51bb
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-28 14:27:44 +0900

    Chapter3_1@Javaのクラス・インスタンス(クラスとは)

---
+++ src/practice/Chapter3_1.java
@@ -0,0 +1,17 @@
+package practice;
+
+// 実行クラス
+public class Chapter3_1 {
+	
+	public static void main(String[] args) {
+		String name = "カレー";
+		String comment = taste1();
+		System.out.println(name);
+		System.out.println(comment);
+	}
+		
+	public static String taste1() {
+		return "スパイシーでおいしい";
+	}
+}
+

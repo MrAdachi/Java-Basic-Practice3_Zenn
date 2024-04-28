b6f463bdab04bc5f8dbe663b512ff0938f4586a4
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-28 21:51:20 +0900

    Chapter8_2@Java条件分岐(三項演算子)

---
+++ src/practice/Chapter8_2.java
@@ -0,0 +1,12 @@
+package practice;
+
+// 実行クラス
+public class Chapter8_2 {
+	public static void main(String[] args) {
+		int val1 = 30;
+		int val2 = 20;
+		
+		String s = val1 > val2 ? "正" : "偽";
+		System.out.println(s);
+	}
+}

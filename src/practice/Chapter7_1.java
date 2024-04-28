79ca261c614308e5079f8c5f23b9e74f561511ce
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-04-28 21:20:28 +0900

    Chapter7_1@Java配列(配列)

---
+++ src/practice/Chapter7_1.java
@@ -0,0 +1,13 @@
+package practice;
+
+// 実行クラス
+public class Chapter7_1 {
+	public static void main(String[] args) {
+		int[] array = {1, 10, 100, 1000, 10000};
+		System.out.println(array[2]);
+		
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+}
